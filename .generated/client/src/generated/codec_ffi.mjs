88e5f58172f1a29483a9fdbc7df4438634a63eed
Dave Rapin <dave@rapin.com>
2026-05-31 20:35:45 -0400

    Remove generated chase target

--- .generated/client/src/generated/codec_ffi.mjs
+++
@@ -1,8 +0,0 @@
-// Generated. Do not edit.
-//
-// Plain ETF constructor registry for the root API graph.
-// TODO: Rust generator emits the client ETF constructor registry here.
-
-export function ensure_decoders() {
-  return true;
-}
--- .generated/server/test/scoreboard_server_test.gleam
+++
@@ -1,9 +0,0 @@
-//// Scoreboard server test entry point.
-////
-//// Gleeunit discovers test modules in this package through this `main`.
-
-import gleeunit
-
-pub fn main() {
-  gleeunit.main()
-}
